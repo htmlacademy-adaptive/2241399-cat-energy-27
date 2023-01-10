7b0b392091981974f92d7db8caf2a89d5945ea8c
Daria Sinko <daria_sinko@bk.ru>
2023-01-10 18:20:13 +0300

    add js for map

--- source/js/main.js
+++ source/js/main.js
@@ -8,3 +8,9 @@ navToggle.addEventListener('click', function () {
 		navMain.classList.toggle('page-header__nav-list--opened');
 		navToggle.classList.toggle('page-header__toggle--opened');
 });
+
+let mapImg = document.querySelector('.map__img');
+let mapIframe = document.querySelector('.map__iframe');
+
+mapImg.classList.add('disabled');
+mapIframe.classList.remove('disabled');
