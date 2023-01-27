b7fb6b0e31fd82298b548c78d0063d250483493e
Daria Sinko <daria_sinko@bk.ru>
2023-01-27 09:12:19 +0300

    fix map

--- source/js/main.js
+++ source/js/main.js
@@ -7,9 +7,3 @@ navMain.classList.add('main-nav--closed');
 navToggle.addEventListener('click', function () {
 	navMain.classList.toggle('main-nav--closed');
 });
-
-let mapImg = document.querySelector('.map__img');
-let mapIframe = document.querySelector('.map__iframe');
-
-mapImg.classList.add('disabled');
-mapIframe.classList.remove('disabled');
