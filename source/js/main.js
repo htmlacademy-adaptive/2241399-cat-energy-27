77ebbb60143ae9fe276dfa01f8b6d57ce9d9ddee
Daria Sinko <daria_sinko@bk.ru>
2023-01-27 09:28:21 +0300

    add slider script

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let slider = document.querySelector(".slider")
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
@@ -7,3 +8,31 @@ navMain.classList.add('main-nav--closed');
 navToggle.addEventListener('click', function () {
 	navMain.classList.toggle('main-nav--closed');
 });
+
+if (slider) {
+	let curtain = slider.querySelector(".slider__curtain")
+	let sliderStyles = getComputedStyle(slider)
+	let curtainPlaceStart
+	let clientX
+
+	function handleGrabCurtain (event) {
+		curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
+		clientX = event.clientX
+		window.addEventListener("pointermove", handleMoveCurtain)
+	}
+
+	function handleMoveCurtain (event) {
+		let deltaX = event.clientX - clientX
+		let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
+		let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
+		slider.style.setProperty("--curtain-place", `${curtainPlace}`)
+	}
+
+	function handleReleaseCurtaine () {
+		window.removeEventListener("pointermove", handleMoveCurtain)
+	}
+
+	window.addEventListener("pointerup", handleReleaseCurtaine)
+
+	curtain.addEventListener("pointerdown", handleGrabCurtain)
+}
